7a651b5bfed6d16286d7e6e1a25e78d628559bfc
R1ck_S4ncherz <121076119+naveen30112002@users.noreply.github.com>
2023-01-07 12:28:10 +0530

    code cleanup

---
+++ portfolio.js
@@ -0,0 +1,264 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Vineeth krishna",
+	title: "Hola, I'm vineeth krishna",
+	description:
+		"I'm a software developer coz it feels kind cool that way",
+	resumeLink:
+		"https://drive.google.com/file/d/108rRUT5_CxKJLP5zR2Zt0a6_QhtNZA40/view?usp=share_link",
+};
+
+export const openSource = {
+	githubUserName: "vineeth-krishna-1234",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	url: "",
+	linkedin: "https://www.linkedin.com/in/vineeth-krishna-407838249/",
+	github: "https://github.com/vineeth-krishna-1234",
+	instagram: "https://www.instagram.com/vineeth._krishna",
+	facebook: "",
+	twitter: "https://twitter.com/Rick_Sancherz",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"The computer was born to solve problems that did not exist before",
+	data: [
+		{
+			title: "Full Stack Development",
+			lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+			skills: [
+			],
+			softwareSkills: [
+				{
+					skillName: "HTML-5",
+					fontAwesomeClassname: "vscode-icons:file-type-html",
+				},
+				{
+					skillName: "CSS-3",
+					fontAwesomeClassname: "vscode-icons:file-type-css",
+				},
+				{
+					skillName: "JavaScript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "Reactjs",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+				{
+					skillName: "mysql",
+					fontAwesomeClassname: "logos:mysql",
+				},
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "Django",
+					fontAwesomeClassname: "vscode-icons:file-type-django",
+				},
+
+				{
+					skillName: "Redux",
+					fontAwesomeClassname: "logos:redux",
+				},
+				{
+					skillName: "wordpress",
+					fontAwesomeClassname: "logos:wordpress",
+				},
+				{
+					skillName: "node",
+					fontAwesomeClassname: "logos:nodejs",
+				},
+				{
+					skillName: "git",
+					fontAwesomeClassname: "logos:git",
+				},
+			],
+		},
+		{
+			title: "Mobile application  developement",
+			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
+			skills: [
+				
+			],
+			softwareSkills: [
+				// ? Check README To get icon details
+				{
+					skillName: "react native",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+				{
+					skillName: "android",
+					fontAwesomeClassname: "logos:android",
+				},
+				{
+					skillName: "javascript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "firebase",
+					fontAwesomeClassname: "logos:firebase",
+				},
+				{
+					skillName: "Github",
+					fontAwesomeClassname: "akar-icons:github-fill",
+				},
+				{
+					skillName: "bootstrap",
+					fontAwesomeClassname: "logos:bootstrap",
+				},
+				{
+					skillName: "git",
+					fontAwesomeClassname: "logos:git",
+				},
+				{
+					skillName: "vs code",
+					fontAwesomeClassname: "logos:visual-studio-code",
+				},
+				
+			],
+		},
+		{
+			title: "Api development",
+			lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+			skills: [
+				
+			],
+			softwareSkills: [
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "django",
+					fontAwesomeClassname: "logos:django",
+				},
+				{
+					skillName: "mysql",
+					fontAwesomeClassname: "logos:mysql",
+				},
+		
+			],
+		},
+	],
+};
+
+export const SkillBars = [
+
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Sri Eshwar",
+		subHeader: "Bachelor of Engineering in Computer Science",
+		duration: "2020 - 2024",
+		desc: "",
+		grade: "CGPA: 8.61",
+		descBullets: [
+			
+		],
+	},
+	{
+		schoolName: "VIVEK VIDYALAYA",
+		subHeader: "HSC",
+		duration: "2019 - 2020",
+		desc: "",
+		grade: "percentage: 85.6",
+		descBullets: [
+			
+		],
+	},
+	{
+		schoolName: "PMG",
+		subHeader: "SSLC",
+		duration: "2017 - 2018",
+		desc: "",
+		grade: "percentage: 93",
+		descBullets: [
+			
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Pentester (Intern)",
+		company: "Defendline",
+		companylogo: "/img/icons/common/defendline.jpeg",
+		date: "2021",
+		desc: "Learned the aspects of system security principles.Used various realtime security tools like metasploit and exploitdb.Audited systems for security vulnerabilities",
+	},
+	{
+		role: "Full Stack Developer (Freelance)",
+		company: "Precot Merdian",
+		companylogo: "/img/icons/common/precot.jpeg",
+		date: "2022",
+		desc: "Engineered modern web application using technologies like reactjs , django .Led team of 6 other colleague over 3 months of development process",
+		
+	},
+	{
+		role: "Full Stack Developer",
+		company: "Gyan Matrix",
+		companylogo: "/img/icons/common/gyanmatrix.jpeg",
+		date: "Present",
+		desc: "Expanded the knowledge and experience of previously learned technologies and came across new technologies,Had a opportunity to work with a lot of people and thereby developing my communication and collaborative skills",
+	},
+	
+];
+
+export const projects = [
+	{
+		name: "Gyan Academy",
+		desc: "A web based online learning  application developed during the internship period.",
+		github: "https://github.com/hari1538radha/Academy",
+		link:"https://gyan-academy-6beyxdote-hari1538radha.vercel.app"
+		
+	},
+	{
+		name: "Meals Unbound",
+		desc: "A realtime mobile applcation for the management of food waste and Donation system",
+		github: "https://github.com/vineeth-krishna-1234/meals-unbound",
+	},
+	{
+		name: "Self Up",
+		desc: "An overall employee management and self apparisal system",
+		github: "https://github.com/vineeth-krishna-1234/cotton_project",
+		link:"https://cotton-project-eee6ut9ia-vineeth-krishna-1234.vercel.app/login"
+	},
+	{
+		name: "Airbnb (clone)",
+		desc: "A simple clone of the airbnb website ,an online marketplace for rental real estate",
+		github: "https://github.com/vineeth-krishna-1234/airbnb-clone",
+	},
+];
+
+
+
+// option to hide or show the ContactUs component
+export const showContactUs = false;
+
+// See object prototype on SEO.jsx page
+export const seoData = {
+	title: "Vineeth Krishna",
+	description:
+		"A passionate Full Stack Web Developer and Blockchain Developer.",
+	author: "vineeth krishna",
+	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+	url: "",
+	keywords: [
+		"vineeth",
+		"vineethkrishna",
+		"developer",
+		"full stack",
+		"Portfolio",
+		"vineeth Portfolio ",
+		"vineeth krishna Portfolio",
+	],
+}
